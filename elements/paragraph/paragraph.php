9c87f51b5632ddcae397fef3ded8e51e9d5278c9
hoda <hoda.hussin@gmail.com>
2015-10-28 13:56:01 +0200

    remove tanslate word

--- elements/paragraph/paragraph.php
+++ elements/paragraph/paragraph.php
@@ -61,7 +61,7 @@ if (!class_exists('Paragraph')) {
         public function draw_template() {
 
             return '<div class="item" >
-            {{item.label |translate}}
+            {{item.label}}
             <div class="item-actions">
             <span class="glyphicon glyphicon-plus" aria-hidden="true" ng-click="list.splice($index, 0, convertItemToObj(item))"></span>
             <span class="glyphicon glyphicon-remove" ng-click="list.splice($index, 1)" aria-hidden="true"></span>
