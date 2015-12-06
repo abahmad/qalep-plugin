e2cec9b77091c5a0776561680d1e78f8556b54cd
hoda <hoda.hussin@gmail.com>
2015-12-06 17:09:14 +0200

    add routor for custom template

--- page_templates/new-templte.php
+++ page_templates/new-templte.php
@@ -1,5 +1,5 @@
 <?php /*
-        * Template Name: page
+        * Template Name: new templte
         *
         * Description: A page template that provides a key component of WordPress as a CMS
         * by meeting the need for a carefully crafted introductory page. The front page template
@@ -9,4 +9,4 @@
         * @subpackage Twenty Thirteen
 
         */?>
-        <?php ob_start();?><!-- <mnbaa_SEPERATOR> []</mnbaa_SEPERATOR>--><?php ob_clean();?><?php do_shortcode("[qalep template id=161]"); ?>
+        <?php ob_start();?><!-- <mnbaa_SEPERATOR> "[\r\n  {\r\n    \"label\": \"[Mnbaa Namozagk Form ID=6]\",\r\n    \"type\": \"shortcode\",\r\n    \"value\": \"[Mnbaa Namozagk Form ID=6]\"\r\n  }\r\n]"</mnbaa_SEPERATOR>--><?php ob_clean();?><?php do_shortcode("[qalep template id=2]"); ?>
